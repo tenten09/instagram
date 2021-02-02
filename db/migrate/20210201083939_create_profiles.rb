2ebd3febd43a68ced7d173e7e40055f4c7aac6a0
Y <>
2021-02-02 11:25:23 +0900

    profile と　userの関連づけ

--- db/migrate/20210201083939_create_profiles.rb
+++ db/migrate/20210201083939_create_profiles.rb
@@ -1,6 +1,7 @@
 class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
+      t.references :user, null: false
       t.string :name
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,15 @@ ActiveRecord::Schema.define(version: 2021_02_01_083939) do
   enable_extension "plpgsql"
 
   create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "account", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
